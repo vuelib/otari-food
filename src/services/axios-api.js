7785c3fc63d6fed194a5eee568b77a03f23b9a5c
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-08-06 23:24:24 +0300

    interceptors added to axios-api service

--- src/services/axios-api.js
+++ src/services/axios-api.js
@@ -15,8 +15,53 @@ class Api {
           'Content-Type': 'application/json'
         }
       });
+
     this.token = options.token;
-    this.refreshToken = options.refreshToken;
+    this.refresh_token = options.refresh_token;
+
+    // Interceptor Request
+    this.client.interceptors.request.use(
+      config => {
+        if (!this.token) return config;
+
+        const newConfig = {
+          headers: {},
+          ...config
+        };
+
+        newConfig.headers.Authorization = `Bearer ${this.token}`;
+        return newConfig;
+      },
+      e => Promise.reject(e)
+    );
+
+    // Interceptor Response
+    this.client.interceptors.response.use(
+      r => r,
+      async error => {
+        if (
+          !this.refresh_token ||
+          error.response.status !== 401 ||
+          error.config.retry
+        ) {
+          throw error;
+        }
+
+        if (!this.refreshRequest) {
+          this.refreshRequest = this.client.post('/login/refresh', {
+            refresh: this.refresh_token
+          });
+        }
+        const data = await this.refreshRequest;
+        this.token = data.token;
+        this.refresh_token = data.refresh_token;
+        const newRequest = {
+          ...error.config,
+          retry: true
+        };
+        return this.client(newRequest);
+      }
+    );
   }
 }
 export default new Api();
