c853bcc73f7084de7a39b74d3c76e8fa66fd00c3
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-08-07 02:50:29 +0300

    sidebar-map component created

---
+++ src/components/Restaurant/SidebarMap.vue
@@ -0,0 +1,119 @@
+<template>
+  <aside class="sidebar-map">
+    <div class="sidebar-map-location">
+      <div class="sidebar-map-location__map-wrapper">
+        <div
+          v-for="point in destinationPoints"
+          :key="point.uuid"
+          class="sidebar-map-location__map"
+          :style="{
+            backgroundImage: `url(https://static-maps.yandex.ru/1.x/?l=map&amp;ll=${point.lon},${point.lat}&amp;size=620,1042&amp;scale=2&amp;z=16&amp;key=AP5KxFsBAAAAr4rWXAIApROgjMkXM-bOIOmMi2amb4pKEysAAAAAAAAAAADynFDWdJDGB1mMpov177fEMLqcCA==)`
+          }"
+        ></div>
+        <div class="sidebar-map-location__pin"></div>
+      </div>
+      <div class="sidebar-map-location__button">
+        <button class="button button--yellow">
+          <span class="arrow-icon"></span>
+          <span>Указать свой адрес</span>
+        </button>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.destinationPoints);
+  },
+  props: {
+    destinationPoints: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  },
+  name: 'SidebarMap'
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar-map {
+  position: fixed;
+  top: 0px;
+  width: 310px;
+  height: calc(100vh - 100px);
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 0px;
+  margin-top: 90px;
+  transform: translateZ(0px);
+}
+.sidebar-map-location {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  &__map-wrapper {
+    flex: 1 1 auto;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+    border-radius: 4px;
+    flex-direction: column;
+    background-color: #f2f2f2;
+    &::before {
+      content: '';
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 2;
+      position: absolute;
+      background: linear-gradient(
+        to top,
+        rgba(0, 0, 0, 0.3) 0%,
+        rgba(0, 0, 0, 0) 49%,
+        rgba(0, 0, 0, 0.3) 100%
+      );
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.05);
+      border-radius: 4px;
+      pointer-events: none;
+    }
+  }
+  &__map {
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    position: absolute;
+    background-size: cover;
+    background-position: center;
+  }
+  &__pin {
+    top: 50%;
+    left: 50%;
+    width: 35px;
+    height: 35px;
+    z-index: 3;
+    position: absolute;
+    transform: translate(-50%, -100%);
+    background-size: contain;
+    background-image: url('~@/assets/svg/black-pin.svg');
+    background-position: center;
+  }
+  &__button {
+    margin-top: 10px;
+  }
+}
+.arrow-icon {
+  width: 18px;
+  height: 18px;
+  display: inline-block;
+  margin-right: 8px;
+  vertical-align: middle;
+  background-size: 100%;
+  background-image: url('~@/assets/svg/locate-icon.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
