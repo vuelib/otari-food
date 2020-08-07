2d787800434453fd726c5b71c202d5c3da2d1003
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-08-07 21:35:25 +0300

    global register scroll-link component

---
+++ src/components/ScrollLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :href="href" @click.prevent="scroll">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    scroll() {
+      document.querySelector(this.href).scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+};
+</script>
