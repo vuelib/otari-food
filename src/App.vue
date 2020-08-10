19acafdb63ed37112a704ef4c974f97cf23c4361
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-08-10 05:07:35 +0300

    entry point component modified

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- App Header -->
+    <AppHeader />
+    <!-- Router View -->
+    <div class="layout">
+      <div class="container">
+        <router-view />
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppHeader from './components/AppHeader';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components: {
+    AppHeader
   }
+};
+</script>
+
+<style lang="scss">
+@import './scss/basic.scss';
+
+.layout {
+  margin: 90px 40px 10px 40px;
+  z-index: 1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ScrollLink from './components/ScrollLink';
+import AppPopup from './components/AppPopup';
 
 Vue.component('scroll-link', ScrollLink);
+Vue.component('app-popup', AppPopup);
+
 Vue.config.productionTip = false;
 
 new Vue({
