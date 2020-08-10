d2016461748c92b273e65e5cc7d3c31762965960
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-08-10 05:04:21 +0300

    create catalog & restaurant pages

---
+++ src/views/CatalogPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="catalog-page">
+    <Promo />
+    <Toolbar />
+    <div class="catalog-content">
+      <CatalogSearch />
+      <PlacesList />
+    </div>
+  </div>
+</template>
+
+<script>
+import Promo from '@/components/Catalog/Promo.vue';
+import Toolbar from '@/components/Catalog/Toolbar.vue';
+import CatalogSearch from '@/components/Catalog/CatalogSearch.vue';
+import PlacesList from '@/components/Catalog/PlacesList.vue';
+
+export default {
+  name: 'CatalogPage',
+  components: {
+    Promo,
+    Toolbar,
+    CatalogSearch,
+    PlacesList
+  }
+};
+</script>
+
+<style lang="scss">
+.catalog-page {
+  border: solid 1px #eeeeee;
+  border-radius: 4px 4px 0 0;
+}
+</style>
