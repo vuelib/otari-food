f645abaa124acc0e289af47c9d2a8063ca978210
TheAdminPro <atagr888@gmail.com>
2020-08-19 18:02:28 +0300

    add get & verification code auth services

--- src/services/auth.js
+++ src/services/auth.js
@@ -10,6 +10,31 @@ export function loginService({ login, password }) {
     .catch(() => {});
 }
 
+export function getCodeFromPhoneNumberService({ phone, device_id }) {
+  return api.client
+    .post('https://client.apis.stage.faem.pro/api/v2/auth/new', {
+      device_id,
+      phone
+    })
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    });
+}
+
+export function checkToEqualVerificationCodeService({ code, device_id }) {
+  console.log(code, device_id);
+  return api.client
+    .post('https://client.apis.stage.faem.pro/api/v2/auth/verification', {
+      device_id,
+      code
+    })
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    });
+}
+
 export function logoutService() {
   api.token = null;
   api.refresh_token = null;
