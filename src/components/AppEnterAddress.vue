48e6e8cfca04641ec0c4b1c35492d6eca7d7bf2d
TheAdminPro <atagr888@gmail.com>
2020-08-26 18:08:10 +0300

    select address logic added

--- src/components/AppEnterAddress.vue
+++ src/components/AppEnterAddress.vue
@@ -11,20 +11,51 @@
           </button>
           <div class="enter-address__address">
             <input
+              v-if="!isSelectAddressNotNull"
+              @input="debounceInput"
+              v-model="enterAddress"
               class="enter-address__address-input"
               autocomplete="off"
               placeholder="Или указать..."
               type="text"
             />
-            <!-- <div class="enter-address__lists">
-              <div class="enter-address__list-item">asd</div>
-              <div class="enter-address__list-item">asd</div>
-              <div class="enter-address__list-item">asd</div>
-              <div class="enter-address__list-item">asd</div>
-              <div class="enter-address__list-item">asd</div>
-            </div> -->
+            <ul
+              v-if="enterAddress.length && !isSelectAddressNotNull"
+              class="suggestions-list"
+            >
+              <li
+                v-for="suggestion in suggestionsAddresses"
+                @click="selectAddress(suggestion)"
+                :key="suggestion.uuid"
+                class="suggestion"
+              >
+                <span class="suggestion__item">
+                  {{ suggestion.unrestricted_value }}
+                  <div class="suggestion__item-label">
+                    Россия, Владикавказ
+                  </div>
+                </span>
+              </li>
+            </ul>
+            <div v-if="isSelectAddressNotNull" class="enter-address__content">
+              <div class="enter-address__content-address">
+                {{ selectedAddress.unrestricted_value }}
+              </div>
+              <div class="enter-address__content-remove"></div>
+            </div>
+            <img
+              v-if="enterAddress.length || isSelectAddressNotNull"
+              @click="clearEnterAddress"
+              src="../assets/svg/clear-icon.svg"
+              class="enter-address__content-remove"
+            />
           </div>
-          <button class="enter-address__ok button button--yellow">Ок</button>
+          <button
+            :disabled="!isSelectAddressNotNull"
+            class="enter-address__ok button button--yellow"
+          >
+            Ок
+          </button>
         </div>
         <div id="enter-address-map" class="enter-address__map"></div>
         <!-- <div class="modal__body"></div> -->
@@ -35,24 +66,90 @@
 </template>
 
 <script>
+import debounce from 'debounce';
 import mapboxgl from 'mapbox-gl';
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZmFlbXRheGkiLCJhIjoiY2pyYXNqZ3RhMHQxNTQ5bjBxMWlvcWF6eSJ9.ISSgNBMdG7idL3ljb2ILTg';
+
+import { createNamespacedHelpers } from 'vuex';
+const { mapActions } = createNamespacedHelpers('location');
 export default {
   mounted() {
+    console.log('mounred enter address');
     this.map = new mapboxgl.Map({
       container: 'enter-address-map',
       style: 'mapbox://styles/faemtaxi/ck0fcruqn1p9o1cnzazi3pli9',
-      center: [44.665976, 43.025234],
-      zoom: 12
+      center: [44.66778, 43.03667],
+      zoom: 18
     });
+    this.marker = new mapboxgl.Marker()
+      .setLngLat([44.665976, 43.025234])
+      .addTo(this.map);
     this.map.addControl(new mapboxgl.NavigationControl());
   },
   methods: {
+    // Select address
+    selectAddress(address) {
+      console.log(address.lon, address.lat, address.value);
+      this.selectedAddress = address;
+      console.log(this.selectedAddress);
+      this.map.flyTo({
+        center: [address.lon, address.lat]
+        // essential: true
+      });
+      this.marker.setLngLat([address.lon, address.lat]);
+      // new mapboxgl.Marker()
+      //   .setLngLat([address.lon, address.lat])
+      //   .addTo(this.map);
+      this.map.zoomTo(18);
+    },
+    // Debounce enter address
+    debounceInput: debounce(async function({ target }) {
+      const data = await this.getAutocompleteAddresses(target.value);
+      this.suggestionsAddresses = data;
+      console.log(data);
+    }, 350),
     delectLocation() {
       console.log('detect');
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            this.map.flyTo({
+              center: [position.coords.longitude, position.coords.latitude]
+            });
+            this.map.zoomTo(18);
+            console.log(position.coords.latitude, position.coords.longitude);
+          },
+          function() {
+            console.log('Not');
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        console.log('Browser does not support Geolocation');
+      }
+    },
+    clearEnterAddress() {
+      (this.selectedAddress = null), (this.enterAddress = '');
+    },
+    ...mapActions(['getAutocompleteAddresses'])
+  },
+  computed: {
+    isEnterAddressNotNull() {
+      return this.enterAddress !== '';
+    },
+    isSelectAddressNotNull() {
+      return this.selectedAddress !== null;
     }
   },
+  data: () => ({
+    map: null,
+    marker: null,
+    enterAddress: '',
+    selectedAddress: null,
+    suggestionsAddresses: [],
+    isShowSuggestions: false
+  }),
   name: 'AppEnterAddress'
 };
 </script>
@@ -60,12 +157,19 @@ export default {
 <style lang="scss">
 .modal {
   &__header {
-    flex: 0 0 auto;
-    width: 100%;
-    height: 45px;
-    position: relative;
-    font-size: 29px;
-    font-weight: 100;
+    // flex: 0 0 auto;
+    // width: 100%;
+    // height: 45px;
+    // position: relative;
+    // font-size: 29px;
+    // font-weight: 100;
+    margin: 5px;
+    padding: 10px 20px;
+    font-size: 24px;
+    font-weight: bold;
+    white-space: pre-line;
+    line-height: 36px;
+    margin-right: 66px;
   }
   &__body {
     max-height: 555px;
@@ -128,6 +232,7 @@ export default {
     border-bottom-right-radius: 0;
   }
   &__address {
+    position: relative;
     flex: 1 1 auto;
     height: 100%;
     min-width: 500px;
@@ -142,20 +247,76 @@ export default {
   &__address-input {
     border-radius: 0 4px 4px 0;
     height: 100%;
-    border: 1px solid #cccccc;
+    border: 1px solid #ccc;
     font-size: 1.125rem;
     box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.2);
     padding: 0px 30px 0 10px;
   }
   &__map {
-    flex: 1 0 auto;
+    // flex: 1 0 auto;
     width: 100%;
     height: 400px;
     position: relative;
+  }
+  &__content {
+    position: relative;
+    height: 44px;
+    display: flex;
+    font-size: 19px;
+    background: $theme-mainColor;
+    padding-top: 2px;
+    align-items: center;
+    font-weight: bold;
+    border-radius: 4px;
+  }
+  &__content-address {
+    flex: 1 1 auto;
+    padding-left: 30px;
+  }
+  &__content-remove {
+    top: 0;
+    right: 6px;
+    height: 100%;
+    cursor: pointer;
+    opacity: 1;
+    position: absolute;
   }
 }
 .mapboxgl-ctrl-bottom-left,
 .mapboxgl-ctrl-bottom-right {
   display: none;
+}
+.suggestions-list {
+  display: flex;
+  background: white;
+  box-shadow: 0 2px 7px 0 rgba(0, 0, 0, 0.15);
+  flex-direction: column;
+  cursor: pointer;
+  max-height: 358px;
+  overflow-y: auto;
+}
+.suggestion {
+  flex: 0 0 auto;
+  color: gray;
+  display: -webkit-box;
+  padding: 6px 12px;
+  overflow: hidden;
+  max-height: 106px;
+  line-height: 22px;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  transition: background-color 0.15s;
+  &:hover {
+    background-color: #eeeeee;
+  }
+  &__item {
+    color: $theme-textColor;
+    font-weight: 100;
+  }
+  &__item-label {
+    color: #aaa;
+    font-size: 14px;
+  }
 }
 </style>
