e6c0c0910e90c55b027414d2b2b3a08361d2b485
TheAdminPro <atagr888@gmail.com>
2020-08-27 17:52:58 +0300

    set current location logic added

--- src/store/modules/location.js
+++ src/store/modules/location.js
@@ -1,14 +1,35 @@
 import { getAutocompleteAddressesService } from '@/services/autocomplete.js';
 
 /* eslint no-unused-vars: */
+const location = JSON.parse(localStorage.getItem('location'));
+console.log('location', location);
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
+  state: {
+    current_location: location ? location : null
+  },
+  mutations: {
+    SET_CURRENT_LOCATION(state, location) {
+      state.current_location = location;
+      console.log(state.current_location);
+      localStorage.setItem('location', JSON.stringify(location));
+    }
+  },
   actions: {
+    setCurrentLocation({ commit }, location) {
+      commit('SET_CURRENT_LOCATION', location);
+    },
     async getAutocompleteAddresses({ commit }, name) {
       return await getAutocompleteAddressesService({ name });
     }
   },
-  getters: {}
+  getters: {
+    getCurrentLocation(state) {
+      return state.current_location;
+    },
+    isCurrentLocationNull(state) {
+      console.log(state.current_location);
+      return state.current_location === null;
+    }
+  }
 };
