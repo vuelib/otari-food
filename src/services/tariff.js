6020cf8cca39e2bd0470a54e56bbdd09a9c6a0f0
TheAdminPro <atagr888@gmail.com>
2020-09-01 18:07:26 +0300

    path get tarrifs service changed

--- src/services/tariff.js
+++ src/services/tariff.js
@@ -1,9 +1,9 @@
 import api from './axios-api';
 
-export function getTariffService({ routeFrom, routeTo, serviceUUID }) {
+export function getTariffsService({ routeFrom, routeTo, serviceUUID }) {
   console.log([routeFrom, routeTo], serviceUUID);
   return api.client
-    .post('/orders/tariff', {
+    .post('/orders/tariffs', {
       routes: [routeFrom, routeTo],
       service_uuid: serviceUUID
     })
