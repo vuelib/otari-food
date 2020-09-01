382a9f1a2e9f14df83d6e583521a8376db046050
TheAdminPro <atagr888@gmail.com>
2020-09-01 12:05:04 +0300

    get tariff service changed

--- src/services/tariff.js
+++ src/services/tariff.js
@@ -1,16 +1,9 @@
 import api from './axios-api';
 
-export function getTariffService({
-  clientUUID,
-  routeFrom,
-  routeTo,
-  serviceUUID = '6b73e9e3-927b-453c-81c4-dfae818291f4'
-}) {
+export function getTariffService({ routeFrom, routeTo, serviceUUID }) {
+  console.log([routeFrom, routeTo], serviceUUID);
   return api.client
     .post('/orders/tariff', {
-      client: {
-        UUID: clientUUID
-      },
       routes: [routeFrom, routeTo],
       service_uuid: serviceUUID
     })
