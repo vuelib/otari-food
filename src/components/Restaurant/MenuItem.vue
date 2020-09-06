b6fc0c80f7957626000ffe02bbfa7a744415cb38
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-09-06 21:49:16 +0300

    add check in cart logic

--- src/components/Restaurant/MenuItem.vue
+++ src/components/Restaurant/MenuItem.vue
@@ -33,10 +33,37 @@
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex';
 export default {
+  created() {
+    this.checkInCart();
+  },
+  updated() {
+    this.checkInCart();
+  },
   computed: {
     isInCart() {
       return this.menuItem.quantity > 0;
+    },
+    ...createNamespacedHelpers('cart').mapGetters([
+      'getCartProducts',
+      'checkOnEqualActiveStoreUUID'
+    ])
+  },
+  methods: {
+    checkInCart() {
+      if (!this.checkOnEqualActiveStoreUUID(this.menuItem.store_uuid)) return;
+      let quant = 0;
+      for (const [key, { menuItem, quantity }] of Object.entries(
+        this.getCartProducts
+      )) {
+        if (menuItem.uuid === this.menuItem.uuid) {
+          quant += quantity;
+        }
+        console.log(key);
+      }
+      if (quant) this.$set(this.menuItem, 'quantity', quant);
+      console.log(this.getCartProducts);
     }
   },
   props: {
