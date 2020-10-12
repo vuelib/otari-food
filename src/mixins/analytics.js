27dd1255d1f6d2964211ecee2fbfea53a8efa96d
Grant Atayan <atagr888@gmail.com>
2020-10-12 17:39:13 +0300

    create analytics api

---
+++ src/mixins/analytics.js
@@ -0,0 +1,68 @@
+export function addProductAnalytics({ name, uuid, price, category, brand }) {
+  window.dataLayer.push({
+    event: 'add',
+    ecommerce: {
+      currencyCode: 'RUB',
+      add: {
+        products: [
+          {
+            name: name, //Название товара
+            id: uuid, //Id товар или артикул
+            price: price, //Стоимость единицы товара
+            category: category, //Раздел меню
+            brand: brand, //Название ресторана
+            quantity: 1 //Количество (всегда 1)
+          }
+        ]
+      }
+    }
+  });
+}
+export function removeProductAnalytics({
+  name,
+  id,
+  price,
+  category,
+  brand,
+  quantity = 1
+}) {
+  window.dataLayer.push({
+    event: 'remove',
+    ecommerce: {
+      currencyCode: 'RUB',
+      remove: {
+        products: [
+          {
+            name: name, //Название товара
+            id: id, //Id товар или артикул
+            price: price, //Стоимость единицы товара
+            category: category, //Раздел меню
+            brand: brand, //Название ресторана
+            quantity: quantity //Количество
+          }
+        ]
+      }
+    }
+  });
+}
+export function purchaseAnalytics({
+  uuid,
+  productsPrice,
+  totalPrice,
+  productsData
+}) {
+  window.dataLayer.push({
+    event: 'purchase',
+    ecommerce: {
+      currencyCode: 'RUB',
+      purchase: {
+        actionField: {
+          id: uuid, //Id заказа
+          revenue: productsPrice, //Cумма транзакции
+          shipping: totalPrice //Cумма доставки
+        },
+        products: [...productsData]
+      }
+    }
+  });
+}
